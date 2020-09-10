e0b3c4d491aa7af63184711879dca5b4f2484744
Wayne Brown <wayne.brown@appdynamics.com>
2020-09-10 11:55:13 -0600

    Final updates for instrumented functions.

--- src/handler2.py
+++ src/handler2.py
@@ -6,6 +6,7 @@ import boto3
 import os
 
 #TODO: Add in AppDynamics tracer decorator
+@appdynamics.tracer
 def lambda_function(event, context):
 
     s3_client = boto3.client('s3')
@@ -17,13 +18,13 @@ def lambda_function(event, context):
     obj_contents = None
 
     #TODO: Add in S3 exit call
-    with appdynamics.ExitCallContextManager(exit_point_type="CUSTOM", exit_point_subtype="Amazon Web Services", identifying_properties={"VENDOR": "S3", "BUCKET_NAME" : os.environ["CANDIDATE_S3_BUCKET"]}) as ec:
+    with appdynamics.ExitCallContextManager(exit_point_type="CUSTOM", exit_point_sub_type="Amazon Web Services", identifying_properties={"VENDOR": "S3", "BUCKET NAME" : os.environ["CANDIDATE_S3_BUCKET"]}) as ec:
         objs = s3_client.list_objects_v2(Bucket = os.environ["CANDIDATE_S3_BUCKET"])['Contents']
 
     idx = randint(0, len(objs) - 1)
 
     obj_key = objs[idx]['Key']    
-    with appdynamics.ExitCallContextManager(exit_point_type="CUSTOM", exit_point_subtype="Amazon Web Services", identifying_properties={"VENDOR": "S3", "BUCKET_NAME" : os.environ["CANDIDATE_S3_BUCKET"]}) as ec:
+    with appdynamics.ExitCallContextManager(exit_point_type="CUSTOM", exit_point_sub_type="Amazon Web Services", identifying_properties={"VENDOR": "S3", "BUCKET NAME" : os.environ["CANDIDATE_S3_BUCKET"]}) as ec:
         obj = s3_client.get_object(Bucket = os.environ["CANDIDATE_S3_BUCKET"], Key = obj_key)
     
     obj_contents = json.loads(obj['Body'].read().decode('utf-8'))
